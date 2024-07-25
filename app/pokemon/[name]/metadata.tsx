c3ab1c071407d34155e89ea606feffacc1521a24
Jorge Perez <info@jperez.ca>
2024-07-25 20:51:08 +0700

    [name]/metadata.tsx file created

---
+++ app/pokemon/[name]/metadata.tsx
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import type { Metadata } from 'next';
+
+export async function generateMetadata({ params }: { params: { name: string } }): Promise<Metadata> {
+  const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${params.name}`);
+  const pokemon = response.data;
+
+  return {
+    title: `${pokemon.name} - PokeNext.js`,
+    description: `Details and stats about ${pokemon.name}.`,
+  };
+}
