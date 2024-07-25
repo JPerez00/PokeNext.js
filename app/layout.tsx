e1a47191ba93dbe9d314ad39c9a1243ee3b396cd
Jorge Perez <info@jperez.ca>
2024-07-25 11:26:07 +0700

    metadata updated, openGraph & twitter data/image added

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,16 +7,46 @@ import Footer from './components/Footer';
 import BackToTop from './components/BackToTop';
 import Theme from './components/theme-provider';
 
+// Update your project URL here
+const baseUrl = 'https://poke-nextjs-opal.vercel.app/'
+// Update Font here
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL(baseUrl),
   title: {
     template: '%s - PokeNext.js',
-    default: 'PokeNext.js - A simple Pokedex page using the RESTful Pokémon API',
+    default: 'PokeNext.js - A simple Pokedex page using the RESTful Pokémon API.',
   },
-  description:
-    'A simple Next.js, Typescript and Tailwind CSS Pokedex page using the RESTful Pokémon API.',
-}
+  description: 'A simple Next.js, Typescript and Tailwind CSS Pokedex page using the RESTful Pokémon API.',
+  authors: [
+    { 
+      name: 'Jorge Perez',
+    },
+  ],
+  openGraph: {
+    title: 'PokeNext.js',
+    description: 'A simple Next.js, Typescript and Tailwind CSS Pokedex page using the RESTful Pokémon API.',
+    url: baseUrl,
+    siteName: 'PokeNext.js',
+    locale: 'en_US',
+    type: 'website',
+    images: [
+      {
+        url: `${baseUrl}public/images/twitter-card.png`,
+        width: 800,
+        height: 600,
+        alt: 'Social Banner',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@yourtwitterhandle',
+    creator: '@yourtwitterhandle',
+    images: [`${baseUrl}public/images/twitter-card.png`],
+  },
+};
 
 export default function RootLayout({
   children,
